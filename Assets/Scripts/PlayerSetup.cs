0b71d144eb9c04e1c147f521f1846c4799a86a7d
Berk Çavuşoğlu <cavusogluberk160@gmail.com>
2025-09-26 21:05:27 +0300

    Multiplayer AR Spinner Game
    Multiplayer Augmented Reality (AR) Game with Unity
    
    This project is a prototype multiplayer AR game built with Unity. It demonstrates how multiple players can interact with shared augmented reality content in real time. The game focuses on:
    
    Synchronizing AR objects across devices
    
    Implementing real-time multiplayer mechanics
    
    Exploring AR user interactions and gameplay
    
    Technologies used: Unity, AR Foundation, Photon/Multiplayer Networking.

---
+++ Assets/Scripts/PlayerSetup.cs
@@ -0,0 +1,43 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using TMPro;
+
+public class PlayerSetup : MonoBehaviourPun
+{
+    public TextMeshProUGUI playerNameText;
+    void Start()
+    {
+        if (photonView.IsMine)
+        {
+            //The player is local player.
+            transform.GetComponent<MovementController>().enabled = true;
+            transform.GetComponent<MovementController>().joystick.gameObject.SetActive(true);
+        }
+        else
+        {
+            transform.GetComponent<MovementController>().enabled = false;
+            transform.GetComponent<MovementController>().joystick.gameObject.SetActive(false);
+        }
+        SetPlayerName();
+    }
+
+    void SetPlayerName()
+    {
+        if (playerNameText != null)
+        {
+            if (photonView.IsMine)
+            {
+                playerNameText.text = "YOU";
+                playerNameText.color = Color.red;
+            }
+            else
+            {
+                playerNameText.text = photonView.Owner.NickName;
+
+            }
+        }
+    }
+}
